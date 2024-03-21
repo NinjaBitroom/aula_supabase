35fe7fa8c293da26b7b50a02e176a1d4302432d0
NinjaBitroom <gabrielbitercourt@gmail.com>
2024-03-21 10:49:04 -0400

    termine a funcionalidade de editar pessoas

--- lib/pages/listar_pessoa_page.dart
+++ lib/pages/listar_pessoa_page.dart
@@ -9,114 +9,103 @@ class ListarPessoaPage extends StatefulWidget {
 }
 
 class _ListarPessoaPageState extends State<ListarPessoaPage> {
-  late Future<List<Map<String, dynamic>>> pessoas;
+  List<Map<String, dynamic>>? pessoas;
+
+  Future<void> _atualizarPessoas() async {
+    final db = OperationSupabaseDB();
+    final listaPessoas = await db.getPessoas();
+    setState(() {
+      pessoas = listaPessoas;
+    });
+  }
 
   @override
   void initState() {
     super.initState();
-    final db = OperationSupabaseDB();
-    pessoas = db.getPessoas();
+    _atualizarPessoas();
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: pessoas,
-      builder: (context, snap) {
-        Widget body;
-        if (snap.hasData) {
-          body = ListView.builder(
-            padding: const EdgeInsets.all(12),
-            itemBuilder: (context, index) => Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: ListTile(
-                leading: IconButton(
-                  onPressed: () {
-                    final nomeController =
-                        TextEditingController(text: snap.data![index]['nome']);
-                    showDialog(
-                      context: context,
-                      builder: (context) => Dialog(
-                        child: Container(
-                          height: 200,
-                          padding: const EdgeInsets.all(12),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              TextField(
-                                controller: nomeController,
-                                decoration: const InputDecoration(
-                                  labelText: 'Editar Nome',
-                                ),
-                              ),
-                              ElevatedButton(
-                                onPressed: () {
-                                  final db = OperationSupabaseDB();
-                                  db.updateNomePessoa(
-                                    snap.data![index]['id'],
-                                    nomeController.text,
-                                  );
-                                  setState(() async {
-                                    (await pessoas)[index]['nome'] =
-                                        nomeController.text;
-                                  });
-                                  Navigator.pop(context);
-                                },
-                                style: const ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStatePropertyAll(Colors.blue),
-                                  foregroundColor:
-                                      MaterialStatePropertyAll(Colors.white),
-                                ),
-                                child: const Text('Salvar'),
-                              )
-                            ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        title: const Text('Listar Pessoa'),
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(12),
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: ListTile(
+            leading: IconButton(
+              onPressed: () {
+                final nomeController =
+                    TextEditingController(text: pessoas?[index]['nome']);
+                showDialog(
+                  context: context,
+                  builder: (context) => Dialog(
+                    child: Container(
+                      height: 200,
+                      padding: const EdgeInsets.all(12),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          TextField(
+                            controller: nomeController,
+                            decoration: const InputDecoration(
+                              labelText: 'Editar Nome',
+                            ),
                           ),
-                        ),
+                          ElevatedButton(
+                            onPressed: () async {
+                              final db = OperationSupabaseDB();
+                              await db.updateNomePessoa(
+                                pessoas?[index]['id'],
+                                nomeController.text,
+                              );
+                              await _atualizarPessoas();
+                              if (!context.mounted) return;
+                              Navigator.pop(context);
+                            },
+                            style: const ButtonStyle(
+                              backgroundColor:
+                                  MaterialStatePropertyAll(Colors.blue),
+                              foregroundColor:
+                                  MaterialStatePropertyAll(Colors.white),
+                            ),
+                            child: const Text('Salvar'),
+                          )
+                        ],
                       ),
-                    );
-                  },
-                  icon: const Icon(Icons.edit),
-                  color: Colors.cyanAccent,
-                ),
-                trailing: IconButton(
-                  onPressed: () {
-                    final db = OperationSupabaseDB();
-                    db.deletePessoa(snap.data![index]['id']);
-                    setState(() {
-                      pessoas = db.getPessoas();
-                    });
-                  },
-                  icon: const Icon(Icons.delete),
-                  color: Colors.redAccent,
-                ),
-                tileColor: Colors.blue,
-                textColor: Colors.white,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(12),
+                    ),
                   ),
-                ),
-                title: Text(snap.data![index]['nome']),
+                );
+              },
+              icon: const Icon(Icons.edit),
+              color: Colors.cyanAccent,
+            ),
+            trailing: IconButton(
+              onPressed: () async {
+                final db = OperationSupabaseDB();
+                await db.deletePessoa(pessoas?[index]['id']);
+                await _atualizarPessoas();
+              },
+              icon: const Icon(Icons.delete),
+              color: Colors.redAccent,
+            ),
+            tileColor: Colors.blue,
+            textColor: Colors.white,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(12),
               ),
             ),
-            itemCount: snap.data!.length,
-          );
-        } else {
-          body = const Center(
-              child: CircularProgressIndicator(
-            color: Colors.blue,
-          ));
-        }
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.blue,
-            foregroundColor: Colors.white,
-            title: const Text('Listar Pessoa'),
+            title: Text(pessoas?[index]['nome']),
           ),
-          body: body,
-        );
-      },
+        ),
+        itemCount: pessoas?.length,
+      ),
     );
   }
 }
